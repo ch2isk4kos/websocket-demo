24037c7a36c48949ed08b42fd00baf5eb4d5a6ca
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-30 18:23:20 -0400

    import environemnt variables and set variable to connect with front-end via mongo client

--- server/main.js
+++ server/main.js
@@ -3,3 +3,6 @@ const cors = require("cors");
 const http = require("http").createServer(express);
 const io = require("socket.io")(http);
 const { MongoClient } = require("mongodb");
+const MONGODB_URI = process.env.MONGO_ATLAS_URI;
+
+const client = new MongoClient(MONGODB_URI);
