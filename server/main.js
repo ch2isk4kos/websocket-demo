86c11697ce7a9b7e14d2462a91066da4adfed26a
ch2isk4kos <ch2isk4kos@gmail.com>
2021-04-30 18:25:09 -0400

    revise constant variable name

--- server/main.js
+++ server/main.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const http = require("http").createServer(express);
 const io = require("socket.io")(http);
 const { MongoClient } = require("mongodb");
-const MONGODB_URI = process.env.MONGO_ATLAS_URI;
 
-const client = new MongoClient(MONGODB_URI);
+const ATLAS_URI = process.env.MONGO_ATLAS_URI;
+
+const client = new MongoClient(ATLAS_URI);
